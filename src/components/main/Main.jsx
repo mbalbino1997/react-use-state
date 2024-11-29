df9651ad4d1075706e42a1e4ee633bb99e3016c8
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-29 20:37:54 +0100

    add logic

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,6 +3,7 @@ import languages from "../../assets/languages"
 import { useState } from 'react'
 export default function () {
     const [count, setCount] = useState(1);
+
     return (
         <main>
             <div>
@@ -10,7 +11,7 @@ export default function () {
             </div>
             <div>
                 {languages.map(language => count === language.id && < div key={language.id} ><h3>{language.title}</h3><p>{language.description}</p></div>)}
-
+                {count === 0 && <div><h3>Nessun linguaggio selezionato</h3></div>}
             </div>
 
         </main >
